37c221c122a7f29fdd91e56449380dbe28ec31f5
Morgan Winbush <morgan.winbush@method.com>
2021-11-14 12:43:12 -0500

    Initial Commit

---
+++ JeniousPOC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JeniousPOC
+//
+//  Created by Morgan Winbush on 11/14/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
